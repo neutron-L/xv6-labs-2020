b7219121d02e4804d696fdef36cf2e37f18a3c08
rda <2055017970@qq.com>
2023-09-18 12:42:37 +0800

    fix a bug: destroy the locks

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -157,4 +157,8 @@ main(int argc, char *argv[])
 
   printf("%d gets, %.3f seconds, %.0f gets/second\n",
          NKEYS*nthread, t1 - t0, (NKEYS*nthread) / (t1 - t0));
+
+  // destroy the lock
+  for (int i = 0; i < NBUCKET;  ++i)
+    pthread_mutex_destroy(&locks[i]); 
 }
